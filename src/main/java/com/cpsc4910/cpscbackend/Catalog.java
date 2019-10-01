dd8f449c21723724b73e67cc03e8dba9d0fee8be
Lawton Stalvey <lstalve@g.clemson.edu>
2019-10-01 15:51:12 -0400

    Admin, Catalog, Product classes added

---
+++ src/main/java/com/cpsc4910/cpscbackend/Catalog.java
@@ -0,0 +1,23 @@
+package com.cpsc4910.cpscbackend;
+
+public class Catalog {
+    private String rule;
+    private String catalogID;
+
+
+    public String getRule() {
+        return rule;
+    }
+
+    public void setRule(String rule) {
+        this.rule = rule;
+    }
+
+    public String getCatalogID() {
+        return catalogID;
+    }
+
+    public void setCatalogID(String catalogID) {
+        this.catalogID = catalogID;
+    }
+}
---
+++ src/main/java/com/cpsc4910/cpscbackend/Product.java
@@ -0,0 +1,49 @@
+package com.cpsc4910.cpscbackend;
+
+public class Product {
+    private int quantity;
+    private String productID;
+    private String name;
+    private String description;
+    private double price;
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
+
+    public String getProductID() {
+        return productID;
+    }
+
+    public void setProductID(String productID) {
+        this.productID = productID;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+}
