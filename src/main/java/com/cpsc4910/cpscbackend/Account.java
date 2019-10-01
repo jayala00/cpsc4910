e6523478db0e35142722afb26ed42a611c8c6569
Lawton Stalvey <lstalve@g.clemson.edu>
2019-10-01 15:20:44 -0400

    Account class added

---
+++ src/main/java/com/cpsc4910/cpscbackend/Account.java
@@ -0,0 +1,31 @@
+package com.cpsc4910.cpscbackend;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Account{
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+
+    private String username;
+    private String password;
+    private String accountID;
+
+    public String getUsername() { return username; }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() { return password; }
+
+    public void setPassword(String password) { this.password = password; }
+
+    public String getAccountID() { return accountID; }
+
+    public void setAccountID(String accountID) { this.accountID = accountID; }
+}
